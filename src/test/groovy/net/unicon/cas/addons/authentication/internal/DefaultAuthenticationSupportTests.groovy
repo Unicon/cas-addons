13b4af03e1529a699f12695f525466e64f501c28
Dmitriy Kopylenko <dima767@gmail.com>
2012-10-11 12:11:00 -0400

    Re-enabled Spock tests as Spock 0.7-groovy-2.0 final has been released

--- src/test/groovy/net/unicon/cas/addons/authentication/internal/DefaultAuthenticationSupportTests.groovy
+++ src/test/groovy/net/unicon/cas/addons/authentication/internal/DefaultAuthenticationSupportTests.groovy
@@ -1,10 +1,11 @@
-//package net.unicon.cas.addons.authentication.internal
+package net.unicon.cas.addons.authentication.internal
 
-//import spock.lang.Specification
 import org.jasig.cas.authentication.principal.Principal
 import org.jasig.cas.authentication.Authentication
 import org.jasig.cas.ticket.TicketGrantingTicket
 import org.jasig.cas.ticket.registry.TicketRegistry
+import spock.lang.Specification
+
 
 /**
  * Spock-based tests for ${link DefaultAuthenticationSupport}
@@ -13,9 +14,9 @@ import org.jasig.cas.ticket.registry.TicketRegistry
  * @author Dmitriy Kopylenko
  * @author Unicon, inc.
  */
-class DefaultAuthenticationSupportTests_X { //extends Specification {
+class DefaultAuthenticationSupportTests extends Specification {
 
-    /*def "testing correctness of DefaultAuthenticationSupport implementation"() {
+    def "testing correctness of DefaultAuthenticationSupport implementation"() {
         given: 'All the dependencies for DefaultAuthenticationSupport are set up as mocks i.e. TicketRegistry, TGT, Authentication, and Principal with attributes'
         Principal principal = Mock()
         principal.attributes >> [attr1: 'val1', attr2: 'val2']
@@ -29,11 +30,11 @@ class DefaultAuthenticationSupportTests_X { //extends Specification {
         TicketRegistry ticketRegistry = Mock()
         ticketRegistry.getTicket('test-tgt', TicketGrantingTicket) >> tgt
 
-        expect: 'DefaultAuthenticationSupport is correctly implemented, that is peeling TGT onion returned by TicketRegistry for a given TGT token'
+        expect: 'DefaultAuthenticationSupport is correctly implemented, that is peeling TGT onion returned by TicketRegistry for a øgiven TGT token'
         DefaultAuthenticationSupport authenticationSupportUnderTest = new DefaultAuthenticationSupport(ticketRegistry)
         //Spock's power asserts
         authenticationSupportUnderTest.getAuthenticationFrom('test-tgt') == authentication
         authenticationSupportUnderTest.getAuthenticatedPrincipalFrom('test-tgt') == principal
         authenticationSupportUnderTest.getPrincipalAttributesFrom('test-tgt') == [attr1: 'val1', attr2: 'val2']
-    }*/
+    }
 }
