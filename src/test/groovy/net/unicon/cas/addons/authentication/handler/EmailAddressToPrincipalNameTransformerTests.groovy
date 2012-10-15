fe21853d2526e93ffd60ca266299c9973afb207d
Dmitriy Kopylenko <dima767@gmail.com>
2012-10-15 09:48:34 -0400

    Using much more compact Spock's data-driven tests

--- src/test/groovy/net/unicon/cas/addons/authentication/handler/EmailAddressToPrincipalNameTransformerTests.groovy
+++ src/test/groovy/net/unicon/cas/addons/authentication/handler/EmailAddressToPrincipalNameTransformerTests.groovy
@@ -14,25 +14,12 @@ class EmailAddressToPrincipalNameTransformerTests extends Specification {
     //Each spec's feature method gets its own instance - for free!
     PrincipalNameTransformer transformerUnderTest = new EmailAddressToPrincipalNameTransformer()
 
-    def "Test transformation of basic user ids that are not in an email format"() {
-        given: 'Basic user id'
-        def notAnEmail = 'userId'
+    def "Test correct transformation logic"() {
+        expect: "Transformer returns non-email values as is, --OR-- returns null for passed in null/empty strings, --OR-- returns part before @ sign for passed in email values"
+        transformerUnderTest.transform(passedInValue) == expectedReturnValue
 
-        expect: 'Transformer to return the same basic id it was passed in'
-        transformerUnderTest.transform(notAnEmail) == 'userId'
-    }
-
-    def "Test transformation of null and empty values"() {
-        expect: 'Transformer to return null if it was passed null or empty String'
-        transformerUnderTest.transform(null) == null
-        transformerUnderTest.transform('') == null
-    }
-
-    def "Test transformation from email values to user ids (without the email address part)"() {
-        given: "User's email address"
-        def emailAddress = 'userId@domain.edu'
-
-        expect: 'Transformer to return the part of an email address before the @ sign'
-        transformerUnderTest.transform(emailAddress) == 'userId'
+        where:
+        passedInValue << ['non_email_string', null, '', 'email_string@example.com']
+        expectedReturnValue << ['non_email_string', null, null, 'email_string']
     }
 }
