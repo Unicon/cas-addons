3e3062d16aa81566759b7449bce0270e665bc874
Dmitriy Kopylenko <dima767@gmail.com>
2012-10-29 13:06:52 -0400

    Added implementation of ShiroHashServicePasswordEncoder

---
+++ src/main/java/net/unicon/cas/addons/authentication/handler/ShiroHashServicePasswordEncoder.java
@@ -0,0 +1,53 @@
+package net.unicon.cas.addons.authentication.handler;
+
+import net.unicon.cas.addons.support.ThreadSafe;
+import org.apache.shiro.crypto.hash.ConfigurableHashService;
+import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.HashRequest;
+import org.jasig.cas.authentication.handler.PasswordEncoder;
+
+/**
+ * Password encoder with salted and more cryptographically stronger hashing support which delegates all the work
+ * to Apache Shiro HashService API.
+ * <p/>
+ * This encoder optionally supports configuration options for <b>digestAlgorithmName</b> , <b>salt</b>, <b>hashIterations</b>
+ * <p/>
+ * If these options are not set at configuration time, the defaults as defined in <code>DefaultHashService</code> are used.
+ *
+ * @author Dmitriy Kopylenko
+ * @author Unicon, inc.
+ * @see <a href="DefaultHashService">http://shiro.apache.org/static/current/apidocs/org/apache/shiro/crypto/hash/DefaultHashService.html</a>
+ * @since 1.0.2
+ */
+@ThreadSafe
+public final class ShiroHashServicePasswordEncoder implements PasswordEncoder {
+
+	private final ConfigurableHashService hashService = new DefaultHashService();
+
+	private String digestAlgorithmName;
+
+	private String salt;
+
+	private int hashIterations;
+
+	@Override
+	public String encode(String password) {
+		if (password == null) {
+			return null;
+		}
+		return this.hashService.computeHash(new HashRequest.Builder().setSalt(this.salt).setSource(password).build()).toHex();
+	}
+
+	/**
+	 * This method is only intended to be called by the infrastructure code managing instances of this class
+	 * once during initialization of this instance
+	 */
+	void init() throws Throwable {
+		if (this.digestAlgorithmName != null) {
+			this.hashService.setHashAlgorithmName(this.digestAlgorithmName);
+		}
+		if (this.hashIterations > 0) {
+			this.hashService.setHashIterations(this.hashIterations);
+		}
+	}
+}
---
+++ src/test/groovy/net/unicon/cas/addons/authentication/handler/ShiroHashServicePasswordEncoderTests.groovy
@@ -0,0 +1,8 @@
+package groovy.net.unicon.cas.addons.authentication.handler
+
+/**
+ * 
+ * @author Dmitriy Kopylenko
+ */
+class ShiroHashServicePasswordEncoderTests {
+}
