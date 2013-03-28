559632e3775759b2588a60b2c144ba92e8775d64
Dmitriy Kopylenko <dima767@gmail.com>
2013-03-28 16:53:11 -0400

    Cas namespace parsers impl work in progress...

--- src/main/java/net/unicon/cas/addons/config/CasNamespaceHandler.java
+++ src/main/java/net/unicon/cas/addons/config/CasNamespaceHandler.java
@@ -1,18 +1,34 @@
 package net.unicon.cas.addons.config;
 
 import com.github.inspektr.audit.support.Slf4jLoggingAuditTrailManager;
+import net.unicon.cas.addons.authentication.internal.DefaultAuthenticationSupport;
+import net.unicon.cas.addons.info.events.CentralAuthenticationServiceEventsPublishingAspect;
 import net.unicon.cas.addons.serviceregistry.JsonServiceRegistryDao;
+import net.unicon.cas.addons.serviceregistry.services.internal.DefaultRegisteredServicesPolicies;
+import net.unicon.cas.addons.support.ResourceChangeDetectingEventNotifier;
+import org.jasig.cas.authentication.AuthenticationManager;
+import org.jasig.cas.authentication.AuthenticationManagerImpl;
+import org.jasig.cas.authentication.handler.AuthenticationHandler;
+import org.jasig.cas.authentication.handler.support.HttpBasedServiceCredentialsAuthenticationHandler;
+import org.jasig.cas.authentication.handler.support.SimpleTestUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.principal.HttpBasedServiceCredentialsToPrincipalResolver;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentialsToPrincipalResolver;
+import org.jasig.cas.monitor.HealthCheckMonitor;
+import org.jasig.cas.monitor.MemoryMonitor;
+import org.jasig.cas.monitor.Monitor;
 import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
-import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
-import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
-import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.beans.factory.support.ManagedList;
+import org.springframework.beans.factory.xml.*;
 import org.w3c.dom.Element;
+
+import java.util.Arrays;
 
 /**
  * TODO: DOCUMENT ME!
+ *
  * @author Dmitriy Kopylenko
  * @author Unicon, inc.
  * @since 1.3
@@ -23,10 +39,16 @@ public class CasNamespaceHandler extends NamespaceHandlerSupport {
 	public void init() {
 		registerBeanDefinitionParser("inspektr-log-files-audit-manager", new InspektrSlf4jAuditTrailManagerBeanDefinitionParser());
 		registerBeanDefinitionParser("json-services-registry", new JsonServicesRegistryDaoBeanDefinitionParser());
+		registerBeanDefinitionParser("resource-change-detector", new ResourceChangeDetectingEventNotifierBeanDefinitionParser());
+		registerBeanDefinitionParser("default-authentication-support", new DefaultAuthenticationSupportBeanDefinitionParser());
+		registerBeanDefinitionParser("default-events-publisher", new DefaultEventsPublisherBeanDefinitionParser());
+		registerBeanDefinitionParser("default-registered-services-policies", new DefaultRegisteredServicesPoliciesBeanDefinitionParser());
+		registerBeanDefinitionParser("default-health-check-monitor", new DefaultHealthCheckMonitorBeanDefinitionParser());
+		registerBeanDefinitionParser("default-test-authentication-manager", new DefaultTestAuthenticationManagerBeanDefinitionParser());
 	}
 
 	/**
-	 * Parses <pre><inspektr-log-files-audit-manager/></pre> elements into bean definitions of type {@link com.github.inspektr.audit.support.Slf4jLoggingAuditTrailManager}
+	 * Parses <pre>inspektr-log-files-audit-manager</pre> elements into bean definitions of type {@link com.github.inspektr.audit.support.Slf4jLoggingAuditTrailManager}
 	 */
 	private static class InspektrSlf4jAuditTrailManagerBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
 
@@ -41,6 +63,9 @@ public class CasNamespaceHandler extends NamespaceHandlerSupport {
 		}
 	}
 
+	/**
+	 * Parses <pre>json-services-registry</pre> elements into bean definitions of type {@link JsonServiceRegistryDao}
+	 */
 	private static class JsonServicesRegistryDaoBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
 
 		@Override
@@ -54,11 +79,152 @@ public class CasNamespaceHandler extends NamespaceHandlerSupport {
 			builder.setInitMethodName("loadServices");
 		}
 
-
-
 		@Override
 		protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
 			return "serviceRegistryDao";
+		}
+	}
+
+	/**
+	 * Parses <pre>resource-change-detector</pre> elements into bean definitions of type {@link ResourceChangeDetectingEventNotifier}
+	 */
+	private static class ResourceChangeDetectingEventNotifierBeanDefinitionParser extends
+			AbstractSingleBeanDefinitionParser {
+
+		@Override
+		protected Class<?> getBeanClass(Element element) {
+			return ResourceChangeDetectingEventNotifier.class;
+		}
+
+		@Override
+		protected void doParse(Element element, BeanDefinitionBuilder builder) {
+			builder.addConstructorArgValue(element.getAttribute("watched-resource"));
+		}
+	}
+
+	/**
+	 * Parses <pre>default-authentication-support</pre> elements into bean definitions of type {@link DefaultAuthenticationSupport}
+	 */
+	private static class DefaultAuthenticationSupportBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+		@Override
+		protected Class<?> getBeanClass(Element element) {
+			return DefaultAuthenticationSupport.class;
+		}
+
+		@Override
+		protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
+			return "authenticationSupport";
+		}
+
+		@Override
+		protected void doParse(Element element, BeanDefinitionBuilder builder) {
+			builder.addConstructorArgReference("ticketRegistry");
+		}
+	}
+
+	/**
+	 * Parses <pre>default-events-publisher</pre> elements into bean definitions of type {@link net.unicon.cas.addons.info.events.CentralAuthenticationServiceEventsPublishingAspect}
+	 */
+	private static class DefaultEventsPublisherBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+		@Override
+		protected Class<?> getBeanClass(Element element) {
+			return CentralAuthenticationServiceEventsPublishingAspect.class;
+		}
+
+		@Override
+		protected boolean shouldGenerateId() {
+			return true;
+		}
+
+		@Override
+		protected void doParse(Element element, BeanDefinitionBuilder builder) {
+			builder.addConstructorArgReference("authenticationSupport");
+		}
+	}
+
+	/**
+	 * Parses <pre>default-registered-services-policies</pre> elements into bean definitions of type {@link net.unicon.cas.addons.serviceregistry.services.internal.DefaultRegisteredServicesPolicies}
+	 */
+	private static class DefaultRegisteredServicesPoliciesBeanDefinitionParser extends
+			AbstractSimpleBeanDefinitionParser {
+
+		@Override
+		protected Class<?> getBeanClass(Element element) {
+			return DefaultRegisteredServicesPolicies.class;
+		}
+
+		@Override
+		protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
+			return "registeredServicesPolicies";
+		}
+	}
+
+	/**
+	 * Parses <pre>default-health-check-monitor</pre> elements into bean definitions of type {@link org.jasig.cas.monitor.HealthCheckMonitor}
+	 */
+	@SuppressWarnings("unchecked")
+	private static class DefaultHealthCheckMonitorBeanDefinitionParser extends AbstractBeanDefinitionParser {
+
+		@Override
+		protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
+			AbstractBeanDefinition memoryMonitorBd = BeanDefinitionBuilder.genericBeanDefinition(MemoryMonitor.class)
+					.addPropertyValue("freeMemoryWarnThreshold", 10).getBeanDefinition();
+
+			ManagedList monitorsList = new ManagedList(1);
+			monitorsList.add(memoryMonitorBd);
+
+			return BeanDefinitionBuilder.genericBeanDefinition(HealthCheckMonitor.class)
+					.addPropertyValue("monitors", monitorsList)
+					.getBeanDefinition();
+		}
+
+		@Override
+		protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
+			return "healthCheckMonitor";
+		}
+	}
+
+	/**
+	 * Parses <pre>default-test-authentication-manager</pre> elements into bean definitions of type {@link org.jasig.cas.authentication.AuthenticationManagerImpl}
+	 */
+	@SuppressWarnings("unchecked")
+	private static class DefaultTestAuthenticationManagerBeanDefinitionParser extends AbstractBeanDefinitionParser {
+
+		@Override
+		protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
+			//CredentialsToPrincipalResolvers list construction
+			AbstractBeanDefinition usernamePasswdPrincipalResolverBd = BeanDefinitionBuilder.genericBeanDefinition(UsernamePasswordCredentialsToPrincipalResolver.class)
+					.addPropertyReference("attributeRepository", "attributeRepository")
+					.getBeanDefinition();
+
+			AbstractBeanDefinition httpBasedPrincipalResolverBd = BeanDefinitionBuilder.genericBeanDefinition(HttpBasedServiceCredentialsToPrincipalResolver.class)
+					.getBeanDefinition();
+
+			ManagedList principalResolversList = new ManagedList(2);
+			principalResolversList.addAll(Arrays.asList(usernamePasswdPrincipalResolverBd, httpBasedPrincipalResolverBd));
+
+			//AuthenticationHandlers list construction
+			AbstractBeanDefinition httpBasedAuthnHandlerBd = BeanDefinitionBuilder.genericBeanDefinition(HttpBasedServiceCredentialsAuthenticationHandler.class)
+					.addPropertyReference("httpClient", "httpClient")
+					.getBeanDefinition();
+
+			AbstractBeanDefinition simpleTestAuthnHandlerBd = BeanDefinitionBuilder.genericBeanDefinition(SimpleTestUsernamePasswordAuthenticationHandler.class)
+					.getBeanDefinition();
+
+			ManagedList authnHandlersList = new ManagedList(2);
+			authnHandlersList.addAll(Arrays.asList(httpBasedAuthnHandlerBd, simpleTestAuthnHandlerBd));
+
+			return BeanDefinitionBuilder.genericBeanDefinition(AuthenticationManagerImpl.class)
+					.addPropertyValue("credentialsToPrincipalResolvers", principalResolversList)
+					.addPropertyValue("authenticationHandlers", authnHandlersList)
+					.getBeanDefinition();
+		}
+
+		@Override
+		protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
+			return "authenticationManager";
 		}
 	}
 }
--- src/test/java/net/unicon/cas/addons/config/CasNamespaceParsersTests.java
+++ src/test/java/net/unicon/cas/addons/config/CasNamespaceParsersTests.java
@@ -3,7 +3,13 @@ package net.unicon.cas.addons.config;
 import static junit.framework.Assert.*;
 
 import com.github.inspektr.audit.support.Slf4jLoggingAuditTrailManager;
+import net.unicon.cas.addons.authentication.internal.DefaultAuthenticationSupport;
+import net.unicon.cas.addons.info.events.CentralAuthenticationServiceEventsPublishingAspect;
 import net.unicon.cas.addons.serviceregistry.JsonServiceRegistryDao;
+import net.unicon.cas.addons.serviceregistry.services.internal.DefaultRegisteredServicesPolicies;
+import net.unicon.cas.addons.support.ResourceChangeDetectingEventNotifier;
+import org.jasig.cas.authentication.AuthenticationManager;
+import org.jasig.cas.monitor.HealthCheckMonitor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +28,64 @@ public class CasNamespaceParsersTests {
 	@Autowired
 	ApplicationContext applicationContext;
 
+	private static final String AUDIT_TRAIL_MANAGER_BEAN_NAME = "auditTrailManager";
+
+	private static final String SERVICE_REGISTRY_DAO_BEAN_NAME = "serviceRegistryDao";
+
+	private static final String RESOURCE_CHANGE_DETECTOR_BEAN_NAME = "testChangeDetector";
+
+	private static final String AUTHENTICATION_SUPPORT_BEAN_NAME = "authenticationSupport";
+
+	private static final String REGISTERED_SERVICES_POLICIES_BEAN_NAME = "registeredServicesPolicies";
+
+	private static final String HEALTH_CHECK_MONITOR_BEAN_NAME = "healthCheckMonitor";
+
+	private static final String AUTHENTICATION_MANAGER_BEAN_NAME = "authenticationManager";
+
 	@Test
 	public void slf4jAuditTrailManagerBeanDefinitionCorrectlyParsed() {
-		assertTrue(applicationContext.containsBean("auditTrailManager"));
+		assertTrue(applicationContext.containsBean(AUDIT_TRAIL_MANAGER_BEAN_NAME));
 		assertTrue(applicationContext.getBeansOfType(Slf4jLoggingAuditTrailManager.class).size() == 1);
 	}
 
 	@Test
 	public void jsonServiceRegistryDaoBeanDefinitionCorrectlyParsed() {
-		assertTrue(applicationContext.containsBean("serviceRegistryDao"));
+		assertTrue(applicationContext.containsBean(SERVICE_REGISTRY_DAO_BEAN_NAME));
 		assertTrue(applicationContext.getBeansOfType(JsonServiceRegistryDao.class).size() == 1);
+	}
+
+	@Test
+	public void resourceChangeDetectingEventNotifierBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.containsBean(RESOURCE_CHANGE_DETECTOR_BEAN_NAME));
+		assertTrue(applicationContext.getBeansOfType(ResourceChangeDetectingEventNotifier.class).size() == 1);
+	}
+
+	@Test
+	public void defaultAuthenticationSupportBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.containsBean(AUTHENTICATION_SUPPORT_BEAN_NAME));
+		assertTrue(applicationContext.getBeansOfType(DefaultAuthenticationSupport.class).size() == 1);
+	}
+
+	@Test
+	public void defaultEventsPublisherBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.getBeansOfType(CentralAuthenticationServiceEventsPublishingAspect.class).size() == 1);
+	}
+
+	@Test
+	public void defaultRegisteredServicesPoliciesBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.containsBean(REGISTERED_SERVICES_POLICIES_BEAN_NAME));
+		assertTrue(applicationContext.getBeansOfType(DefaultRegisteredServicesPolicies.class).size() == 1);
+	}
+
+	@Test
+	public void defaultHealthCheckMonitorBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.containsBean(HEALTH_CHECK_MONITOR_BEAN_NAME));
+		assertTrue(applicationContext.getBeansOfType(HealthCheckMonitor.class).size() == 1);
+	}
+
+	@Test
+	public void defaultTestAuthenticationManagerBeanDefinitionCorrectlyParsed() {
+		assertTrue(applicationContext.containsBean(AUTHENTICATION_MANAGER_BEAN_NAME));
+		assertTrue(applicationContext.getBeansOfType(AuthenticationManager.class).size() == 1);
 	}
 }
