22bc3adeaf14761650be174a36f51c151cb691ed
Dmitriy Kopylenko <dima767@gmail.com>
2013-06-15 18:51:02 -0400

    added the ability to `turn on` ReloadableServicesManagerSuppressionAspect's reloading suppression by means of custom `<cas:disable-default-registered-services-reloading/>`

--- src/main/java/net/unicon/cas/addons/serviceregistry/ReloadableServicesManagerSuppressionAspect.java
+++ src/main/java/net/unicon/cas/addons/serviceregistry/ReloadableServicesManagerSuppressionAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * An aspect to suppress <code>reload()</code> calls on the default CAS' implementation of <code>ReloadableServicesManager</code> instance
@@ -19,16 +20,31 @@ import org.slf4j.LoggerFactory;
 @Aspect
 public final class ReloadableServicesManagerSuppressionAspect {
 
-	private static final Logger logger = LoggerFactory.getLogger(ReloadableServicesManagerSuppressionAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(ReloadableServicesManagerSuppressionAspect.class);
 
-	@Pointcut("execution(public void org.jasig.cas.services.ReloadableServicesManager+.reload(..))")
-	public void reloadMethodExecution(){}
+    /**
+     * This Aspect has no effect by default and needs to be explicitly <i>switched on</i> by setting this property to <code>true</code>
+     */
+    private boolean on;
 
-	@Pointcut("cflowbelow(execution(* net.unicon.cas.addons.serviceregistry.JsonServiceRegistryDao.*(..)))")
-	public void calledByCasAddonsCode(){}
+    public void setOn(boolean on) {
+        this.on = on;
+    }
 
-	@Around("reloadMethodExecution() && !calledByCasAddonsCode()")
-	public void suppressDefaultCasReloadCall(ProceedingJoinPoint target) {
-		logger.debug("Suppressing default reloading behavior of [{}]...", target.getSignature().toString());
-	}
+    @Pointcut("execution(public void org.jasig.cas.services.ReloadableServicesManager+.reload(..))")
+    public void reloadMethodExecution() {
+    }
+
+    @Pointcut("cflowbelow(execution(* net.unicon.cas.addons.serviceregistry.JsonServiceRegistryDao.*(..)))")
+    public void calledByCasAddonsCode() {
+    }
+
+    @Around("reloadMethodExecution() && !calledByCasAddonsCode()")
+    public void suppressDefaultCasReloadCall(ProceedingJoinPoint target) throws Throwable {
+        if (on) {
+            logger.debug("Suppressing default reloading behavior of [{}]...", target.getSignature().toString());
+            return;
+        }
+        target.proceed();
+    }
 }
